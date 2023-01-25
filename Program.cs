042ba99004b5ad5a3d821b14d74c009cccec4ba6
«Kristina» <kristinka007_92@mail.ru>
2023-01-25 13:48:27 +0300

    Задача 47

--- Program.cs
+++ Program.cs
@@ -10,32 +10,43 @@ Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
-double[,] resultArray = new double[rows, columns];
-FillArrayRandomNumbers(resultArray);
-PrintArray(resultArray);
+Console.WriteLine();
+PrintArray(GetArray(rows, columns, -10, 10));
 
-void FillArrayRandomNumbers(double[,] array)
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    double[,] FillArrayRandom = new double[m, n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10));
-        }
+        for (int j = 0; j < n; j++)
+        { FillArrayRandom[i, j] = Math.Round((new Random().NextDouble() * (maxValue - minValue) + minValue),1); }
     }
+    return FillArrayRandom;
 }
 
-void PrintArray(double[,] array)
+void PrintArray(double[,] Array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < Array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + " ");
+            Console.Write(Array[i, j] + " ");
         }
         Console.WriteLine("");  
     }
 }         
+
+/*Задача 50. Напишите программу, которая на вход принимает число, и проверяет есть ли такое число в двумерном массиве, 
+а также возвращает сообщение о том, что оно найдено или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет  */
+
+
 
 
 
