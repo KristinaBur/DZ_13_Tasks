39da152b615a552effc7456f0d477f63a77ea4b1
«Kristina» <kristinka007_92@mail.ru>
2023-01-25 17:26:11 +0300

    Задача 50

--- Program.cs
+++ Program.cs
@@ -5,14 +5,12 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9    */
 
- 
-Console.WriteLine("Введите количество строк: ");
+/*Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 PrintArray(GetArray(rows, columns, -10, 10));
-
 
 double[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -35,7 +33,7 @@ void PrintArray(double[,] Array)
         }
         Console.WriteLine("");  
     }
-}         
+}         */
 
 /*Задача 50. Напишите программу, которая на вход принимает число, и проверяет есть ли такое число в двумерном массиве, 
 а также возвращает сообщение о том, что оно найдено или же указание, что такого элемента нет.
@@ -46,7 +44,56 @@ void PrintArray(double[,] Array)
 8 4 2 4
 17 -> такого числа в массиве нет  */
 
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
 
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
 
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
 
+Console.Write("Введите число, которое ищите: ");
+int number = int.Parse(Console.ReadLine()!);
 
+if (ArrayForeach(number, array) == true)
+{
+    Console.WriteLine($"Число {number} найдено в массиве");
+}
+else Console.WriteLine($"Число {number} не найдено в массиве");
+
+// -------------------Методы---------------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool ArrayForeach(int num, int[,] Array)
+{
+    foreach (int el in Array)
+    {
+        if (num == el) return true;
+
+    }
+    return false;
+}
